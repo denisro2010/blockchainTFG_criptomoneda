b8b9b51a10b3c5190bb6e01fbaede7fedf383f5b
denis <denisro2010@gmail.com>
2019-03-27 20:12:16 +0100

    .

--- BlockchainTFG_criptomoneda/src/vista/cccccc.java
+++ BlockchainTFG_criptomoneda/src/vista/cccccc.java
@@ -40,7 +40,7 @@ public class cccccc extends JFrame {
 	 */
 	public cccccc() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 850, 800);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
